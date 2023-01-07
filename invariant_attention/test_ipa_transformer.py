74147f2b291887bae263d2779b510ede0a053744
Rishit Dagli <rishit.dagli@gmail.com>
2023-01-07 05:51:06 +0000

    Format with black

--- invariant_attention/test_ipa_transformer.py
+++ invariant_attention/test_ipa_transformer.py
@@ -9,23 +9,26 @@ class IPATransformerTest(tf.test.TestCase):
     def setUp(self):
         super(IPATransformerTest, self).setUp()
 
-        seq           = tf.random.normal((1, 256, 32))
+        seq = tf.random.normal((1, 256, 32))
         pairwise_repr = tf.random.normal((1, 256, 256, 32))
-        mask          = tf.ones((1, 256), dtype = tf.bool)
-        translations  = tf.zeros((1, 256, 3))         
+        mask = tf.ones((1, 256), dtype=tf.bool)
+        translations = tf.zeros((1, 256, 3))
 
-        model = IPATransformer(dim=32,
+        model = IPATransformer(
+            dim=32,
             depth=2,
-            num_tokens = None,
-            predict_points = False,
-            detach_rotations = True,)
+            num_tokens=None,
+            predict_points=False,
+            detach_rotations=True,
+        )
 
-        self.outputs = model(single_repr=seq,
-            translations = translations,
-            quaternions = tf.random.normal((1,256,4)),
-            pairwise_repr = pairwise_repr,
-            mask = mask)
-
+        self.outputs = model(
+            single_repr=seq,
+            translations=translations,
+            quaternions=tf.random.normal((1, 256, 4)),
+            pairwise_repr=pairwise_repr,
+            mask=mask,
+        )
 
     def test_shape_and_rank_1(self):
         self.assertEqual(tf.rank(self.outputs[0]), 3)
@@ -38,6 +41,7 @@ class IPATransformerTest(tf.test.TestCase):
     def test_shape_and_rank_3(self):
         self.assertEqual(tf.rank(self.outputs[2]), 3)
         self.assertShapeEqual(np.zeros((1, 256, 4)), self.outputs[2])
+
 
 if __name__ == "__main__":
     tf.test.main()
