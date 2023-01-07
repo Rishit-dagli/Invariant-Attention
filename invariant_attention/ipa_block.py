592c50f013f58d4bf8f337ee50e78d789e20ab27
Rishit Dagli <rishit.dagli@gmail.com>
2023-01-07 03:51:28 +0000

    Implement IPA Block

--- invariant_attention/ipa_block.py
+++ invariant_attention/ipa_block.py
@@ -1,4 +1,4 @@
-from .invariant_attention import InvariantAttention
+from .invariant_attention import InvariantPointAttention
 import tensorflow as tf
 
 class FeedForward(tf.keras.layers.Layer):
@@ -31,3 +31,39 @@ class FeedForward(tf.keras.layers.Layer):
     
     def call(self, inputs):
         return self.layers(inputs)
+
+class IPABlock(tf.keras.layers.Layer):
+    def __init__(
+        self,
+        dim,
+        ff_mult=1,
+        ff_num_layers=3,
+        post_norm=True,
+        post_attn_dropout=0.,
+        post_ff_dropout=0.,
+        **kwargs
+    ):
+        super(IPABlock, self).__init__()
+        self.post_norm = post_norm
+
+        self.attn_norm = tf.keras.layers.LayerNormalization(axis = -1)
+        self.attn = InvariantPointAttention(dim=dim, **kwargs)
+        self.post_attn_dropout = tf.keras.layers.Dropout(post_attn_dropout)
+
+        self.ff_norm = tf.keras.layers.LayerNormalization(axis = -1)
+        self.ff = FeedForward(dim, mult=ff_mult, num_layers=ff_num_layers)
+        self.post_ff_dropout = tf.keras.layers.Dropout(post_ff_dropout)
+
+    def call(self, x, **kwargs):
+        post_norm = self.post_norm
+
+        attn_input = x if post_norm else self.attn_norm(x)
+        x = self.attn(attn_input, **kwargs) + x
+        x = self.post_attn_dropout(x)
+        x = self.attn_norm(x) if post_norm else x
+
+        ff_input = x if post_norm else self.ff_norm(x)
+        x = self.ff(ff_input) + x
+        x = self.post_ff_dropout(x)
+        x = self.ff_norm(x) if post_norm else x
+        return x
