a45bbe7647bf73e34ca2b99d60bb1a78c53a197c
Rishit Dagli <rishit.dagli@gmail.com>
2023-01-07 05:41:26 +0000

    Format with black

--- invariant_attention/test_ipa_block.py
+++ invariant_attention/test_ipa_block.py
@@ -10,15 +10,15 @@ class InvariantPointAttentionTest(tf.test.TestCase):
         super(InvariantPointAttentionTest, self).setUp()
 
         self.block = IPABlock(
-            dim = 64,
-            heads = 8,
-            scalar_key_dim = 16,
-            scalar_value_dim = 16,
-            point_key_dim = 4,
-            point_value_dim = 4
+            dim=64,
+            heads=8,
+            scalar_key_dim=16,
+            scalar_value_dim=16,
+            point_key_dim=4,
+            point_value_dim=4,
         )
 
-        self.seq           = tf.random.normal((1, 256, 64))
+        self.seq = tf.random.normal((1, 256, 64))
         self.pairwise_repr = tf.random.normal((1, 256, 256, 64))
         self.mask = tf.ones((1, 256), dtype=tf.bool)
         self.rotations = repeat(tf.eye(3), "... -> b n ...", b=1, n=256)
@@ -26,14 +26,16 @@ class InvariantPointAttentionTest(tf.test.TestCase):
 
         outputs = self.block(
             self.seq,
-            pairwise_repr = self.pairwise_repr,
-            rotations = self.rotations,
-            translations = self.translations,
-            mask = self.mask
+            pairwise_repr=self.pairwise_repr,
+            rotations=self.rotations,
+            translations=self.translations,
+            mask=self.mask,
         )
 
         updates = tf.keras.layers.Dense(6)(outputs)
-        self.quaternion_update, self.translation_update = tf.split(updates, num_or_size_splits=2, axis=-1)
+        self.quaternion_update, self.translation_update = tf.split(
+            updates, num_or_size_splits=2, axis=-1
+        )
 
     def test_shape_and_rank_translation_update(self):
         self.assertEqual(tf.rank(self.translation_update), 3)
